c516246c93e1cda79e72a79a4446d09c6cf5a78c
Vince Andrew Pimentel <contact@vincep.me>
2019-01-18 21:54:46 +0000

    Fixed styling

--- app/models/preset.rb
+++ app/models/preset.rb
@@ -15,9 +15,49 @@ class Preset < ActiveRecord::Base
         url: "https://jukehost.co.uk/api/audio/9cc957e015992dfb78334f2cb34a630953a7aaf0/06259a051ee",
         icon: "fas fa-umbrella-beach"
       },
+      river: {
+        url: "",
+        icon: "fas fa-water"
+      },
+      wind: {
+        url: "",
+        icon: "fas fa-wind"
+      },
+      leaf: {
+        url: "",
+        icon: "fas fa-leaf"
+      },
       fire: {
         url: "https://jukehost.co.uk/api/audio/9cc957e015992dfb78334f2cb34a630953a7aaf0/f22c3aaed90",
         icon: "fas fa-fire"
+      },
+      bird: {
+        url: "",
+        icon: "fas fa-dove"
+      },
+      cricket: {
+        url: "",
+        icon: ""
+      },
+      train: {
+        url: "",
+        icon: "fas fa-subway"
+      },
+      crowd: {
+        url: "",
+        icon: "fas fa-coffee"
+      },
+      white: {
+        url: "",
+        icon: ""
+      },
+      pink: {
+        url: "",
+        icon: ""
+      },
+      brown: {
+        url: "",
+        icon: ""
       }
     }
   end
