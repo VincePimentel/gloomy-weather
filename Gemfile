bec220da7c223b2ceb7495c89bab2fc84d6c6b68
Vince Andrew Pimentel <contact@vincep.me>
2019-01-24 19:51:08 +0000

    Add pg gem, database.yml, edit environment.rb

--- Gemfile
+++ Gemfile
@@ -8,13 +8,14 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem "sinatra"
 gem "activerecord", :require => "active_record"
 gem "sinatra-activerecord", :require => "sinatra/activerecord"
+gem "sqlite3", :group => :development
+gem "tux", :group => :development
+gem "pg", :group => :production
 gem "rake"
 gem "require_all"
-gem "sqlite3"
 gem "thin"
 gem "shotgun"
 gem "pry"
 gem "bcrypt"
-gem "tux"
 gem "rack-flash3"
 gem "sinatra-flash"
--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,29 @@ ENV["SESSION_SECRET"] = "caa61a414fbf122a35940ee0c424ad6968f1809138598df039cd7d0
 require "bundler/setup"
 Bundler.require
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/development.sqlite"
-)
+configure :development do
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  )
+end
+
+configure :production do
+  db = URI.parse(ENV["DATABASE_URL"] || "postgres:///localhost/mydb")
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == "postgres" ? "postgresql" : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => "utf8"
+  )
+ end
 
 require_all "app"
